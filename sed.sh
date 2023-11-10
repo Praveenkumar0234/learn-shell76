7031acfda54496f969db308f3b84719fa9638bb8
praveen kumar <praveenkumar.tst@gmail.com>
2023-11-10 19:17:25 +0530

    learning ansible

---
+++ sed.sh
@@ -0,0 +1,19 @@
+1. search and replace
+2. delete th lines
+3. Add your lines
+4. replace lines
+
+sed 's/nologin/yeslogin/' test.txt
+
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+adm:x:3:4:adm:/var/adm:/sbin/nologin
+lp:x:4:7:lp:/var/spool/lpd:/sbin/nologin
+sync:x:5:0:sync:/sbin:/bin/sync
+shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown
+halt:x:7:0:halt:/sbin:/sbin/halt
+mail:x:8:12:mail:/var/spool/mail:/sbin/nologin
+operator:x:11:0:operator:/root:/sbin/nologin
+
+sed 's/nologin/yeslogin/'
